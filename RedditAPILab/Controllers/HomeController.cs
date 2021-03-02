ce75d0e8663c6e46f28cfcb8073114ec7126b7a7
Nate <nsdavis@mtu.edu>
2021-03-01 20:38:07 -0500

    Completed Lab Reqs

---
+++ RedditAPILab/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RedditAPILab.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RedditAPILab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private RedditDAL rdd = new RedditDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult Error(string message)
+        //{
+        //    return View(message);
+        //}
+
+        public IActionResult TopTen(string subreddit)
+        {
+            TempData["sub"] = subreddit;
+
+            SubRedditRoot srr = rdd.ConvertToSubReddit(subreddit);
+            
+            Child[] childrens = srr.data.children;
+            List<Child> c = childrens.ToList();
+
+            return View(c);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RedditAPILab/Models/RedditDAL.cs
@@ -0,0 +1,38 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RedditAPILab.Models
+{
+    public class RedditDAL
+    {
+        public string GetData(string subreddit)
+        {
+            string url = @$"https://www.reddit.com/r/{subreddit}/.json";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+
+            string JSON = rd.ReadToEnd();
+
+            return JSON;
+            
+        }
+
+        public SubRedditRoot ConvertToSubReddit(string subreddit)
+        {
+            string subRedditData = GetData(subreddit);
+            SubRedditRoot s = new SubRedditRoot();
+            s = JsonConvert.DeserializeObject<SubRedditRoot>(subRedditData);
+            
+            return s;
+        }
+    }
+}
